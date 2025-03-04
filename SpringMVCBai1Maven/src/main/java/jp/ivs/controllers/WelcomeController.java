7f46675c8aa37b2d4cdb1b7a5eb44fe682699daa
TruongVoXuan <truong.75.cvan@gmail.com>
2025-03-04 09:04:27 +0700

    complete vd_springmaven

--- SpringMVCBai1Maven/src/main/java/jp/ivs/controllers/WelcomeController.java
+++ SpringMVCBai1Maven/src/main/java/jp/ivs/controllers/WelcomeController.java
@@ -1,10 +1,14 @@
 package jp.ivs.controllers;
 
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class WelcomeController {
-@RequestMapping("/welcome")
-public String welcome() {
-	return "viewWelcome";
-}
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+        return "viewWelcome";
+    }
 }
