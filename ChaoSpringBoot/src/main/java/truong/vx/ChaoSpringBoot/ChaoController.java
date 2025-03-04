37c6f483e48f858b39405a54944a33ecd180b065
TruongVoXuan <truong.75.cvan@gmail.com>
2025-03-04 10:50:35 +0700

    complete_vdSpring_Boot

--- ChaoSpringBoot/src/main/java/truong/vx/ChaoSpringBoot/ChaoController.java
+++ ChaoSpringBoot/src/main/java/truong/vx/ChaoSpringBoot/ChaoController.java
@@ -1,7 +1,8 @@
 package truong.vx.ChaoSpringBoot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller  //Bao? rang` day la` 1 lop' controller
 public class ChaoController {
@@ -9,9 +10,10 @@ public class ChaoController {
 	//action method
 	//URL goi action nay`
 
-	@RequestMapping("/chao")
-	public String xin_Chao() {
+	@GetMapping("/")
+	public String xin_Chao(ModelMap m) {
 		//code xu? ly' o? day
+		m.addAttribute("tb","Du lieu thong bao xin chao");
 		return "helloView";
 	}
 }
