2d7a7bbabadc82bf6e8b89dd29db7e59531e4b57
TruongVoXuan <truong.75.cvan@gmail.com>
2025-04-01 09:21:00 +0700

    push-lan2

--- VoXuanTruong_thiGK/src/main/java/thiGK/ntu64132791/VoXuanTruong_thiGK/Model/Page.java
+++ VoXuanTruong_thiGK/src/main/java/thiGK/ntu64132791/VoXuanTruong_thiGK/Model/Page.java
@@ -1,5 +1,47 @@
 package thiGK.ntu64132791.VoXuanTruong_thiGK.Model;
 
 public class Page {
+    private int id;
+    private String pageName;
+    private String keyword;
+    private String content;
 
+    public Page(int id, String pageName, String keyword, String content) {
+        this.id = id;
+        this.pageName = pageName;
+        this.keyword = keyword;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPageName() {
+        return pageName;
+    }
+
+    public void setPageName(String pageName) {
+        this.pageName = pageName;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
