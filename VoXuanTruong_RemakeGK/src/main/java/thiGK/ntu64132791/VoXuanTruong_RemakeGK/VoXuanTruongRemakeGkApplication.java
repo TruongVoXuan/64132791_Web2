088d06bb5513a117fa4c3f9ebe7db6d61038485f
TruongVoXuan <truong.75.cvan@gmail.com>
2025-04-14 21:21:41 +0700

    tạo lại dự án thi GK

---
+++ VoXuanTruong_RemakeGK/src/main/java/thiGK/ntu64132791/VoXuanTruong_RemakeGK/VoXuanTruongRemakeGkApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64132791.VoXuanTruong_RemakeGK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoXuanTruongRemakeGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoXuanTruongRemakeGkApplication.class, args);
+	}
+
+}
---
+++ VoXuanTruong_RemakeGK/src/test/java/thiGK/ntu64132791/VoXuanTruong_RemakeGK/VoXuanTruongRemakeGkApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64132791.VoXuanTruong_RemakeGK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoXuanTruongRemakeGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
